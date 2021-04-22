495dced050b2f984cafcd7c862bb938d9f78b619
Paulo Vieira <paullocostta86@gmail.com>
2021-04-22 14:44:48 -0300

    refactor: send questions to challenge page

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -67,7 +67,8 @@ class _HomePageState extends State<HomePage> {
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) => ChallengePage()),
+                                      builder: (context) => ChallengePage(
+                                          questions: e.questions)),
                                 );
                               },
                             ))
