5e3956c4733e6648989c3ae06896b2f8cbee2699
Paulo Vieira <paullocostta86@gmail.com>
2021-04-22 12:13:51 -0300

    feat: add buttons navigation bar

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -2,6 +2,8 @@ import 'package:devquiz/challenge/widgets/question_indicator/question_indicator_
 import 'package:devquiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:flutter/material.dart';
 
+import 'widgets/next_button/next_button_widget.dart';
+
 class ChallengePage extends StatefulWidget {
   ChallengePage({Key? key}) : super(key: key);
 
@@ -20,7 +22,31 @@ class _ChallengePageState extends State<ChallengePage> {
           child: QuestionIndicatorWidget(),
         ),
       ),
-      body: QuizWidget(title: "O que o Flutter faz em sua totalidade?"),
+      body: QuizWidget(
+        title: "O que o Flutter faz em sua totalidade?",
+      ),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                  child: NextButtonWidget.white(
+                label: "Pular",
+                onTap: () {},
+              )),
+              SizedBox(width: 7),
+              Expanded(
+                  child: NextButtonWidget.green(
+                label: "Confirmar",
+                onTap: () {},
+              )),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
