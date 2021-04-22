2b3cec69e55d6c60f9a50ed05ec06aec12a921d6
Paulo Vieira <paullocostta86@gmail.com>
2021-04-22 14:18:10 -0300

    fix: typo

--- lib/shared/models/question_model.dart
+++ lib/shared/models/question_model.dart
@@ -4,23 +4,23 @@ import 'awnser_model.dart';
 
 class QuestionModel {
   final String title;
-  final List<AnswerModel> answers;
+  final List<AnswerModel> awnsers;
 
-  QuestionModel({required this.title, required this.answers})
-      : assert(answers.length == 4);
+  QuestionModel({required this.title, required this.awnsers})
+      : assert(awnsers.length == 4);
 
   Map<String, dynamic> toMap() {
     return {
       'title': title,
-      'answers': answers.map((x) => x.toMap()).toList(),
+      'awnsers': awnsers.map((x) => x.toMap()).toList(),
     };
   }
 
   factory QuestionModel.fromMap(Map<String, dynamic> map) {
     return QuestionModel(
       title: map['title'],
-      answers: List<AnswerModel>.from(
-          map['answers']?.map((x) => AnswerModel.fromMap(x))),
+      awnsers: List<AnswerModel>.from(
+          map['awnsers']?.map((x) => AnswerModel.fromMap(x))),
     );
   }
 
