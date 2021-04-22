12ce1c6e9ae4c97535363323830982ba348fb5da
Paulo Vieira <paullocostta86@gmail.com>
2021-04-22 16:36:28 -0300

    refactor: pass awnser to AwnserWidget

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,9 +1,10 @@
 import 'package:devquiz/challenge/widgets/awnser/awnser_widget.dart';
 import 'package:devquiz/core/core.dart';
 import 'package:devquiz/shared/models/question_model.dart';
+import 'package:devquiz/shared/models/awnser_model.dart';
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
+class QuizWidget extends StatefulWidget {
   final QuestionModel question;
   const QuizWidget({
     Key? key,
@@ -11,25 +12,36 @@ class QuizWidget extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AnswerModel awnser(int index) => widget.question.awnsers[index];
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-        child: Column(children: [
-      SizedBox(height: 64),
-      Text(
-        question.title,
-        style: AppTextStyles.heading,
-      ),
-      SizedBox(
-        height: 24,
-      ),
-      ...question.awnsers
-          .map(
-            (e) => AwnserWidget(
-              isRight: e.isRight,
-              title: e.title,
-            ),
-          )
-          .toList(),
-    ]));
+        child: Column(
+      children: [
+        SizedBox(height: 64),
+        Text(
+          widget.question.title,
+          style: AppTextStyles.heading,
+        ),
+        SizedBox(
+          height: 24,
+        ),
+        for (var i = 0; i < widget.question.awnsers.length; i++)
+          AwnserWidget(
+              awnser: awnser(i),
+              isSelected: indexSelected == i,
+              onTap: () {
+                indexSelected = i;
+                setState(() {});
+              }),
+      ],
+    ));
   }
 }
