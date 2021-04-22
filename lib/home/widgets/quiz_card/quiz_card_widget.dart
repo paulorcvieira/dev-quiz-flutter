e6a394f2f5b8f45910ec562e88c6b0d0edc4b4aa
Paulo Vieira <paullocostta86@gmail.com>
2021-04-21 21:12:24 -0300

    feat: alter quiz card to receive data

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,42 +1,63 @@
-import 'package:devquiz/core/core.dart';
-import 'package:devquiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
+import 'package:devquiz/core/core.dart';
+import 'package:devquiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
+
 class QuizCardWidget extends StatelessWidget {
-  const QuizCardWidget({Key? key}) : super(key: key);
+  final String title;
+  final String completed;
+  final double percent;
+
+  const QuizCardWidget(
+      {Key? key,
+      required this.title,
+      required this.completed,
+      required this.percent})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-            border: Border.fromBorderSide(BorderSide(color: AppColors.border)),
-            color: AppColors.white,
-            borderRadius: BorderRadius.circular(10)),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+          border: Border.fromBorderSide(BorderSide(
+            color: AppColors.border,
+          )),
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(10)),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Container(
+          height: 40,
+          width: 40,
+          child: Image.asset(AppImages.blocks),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          title,
+          style: AppTextStyles.heading15,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
           children: [
-            Container(
-              width: 40,
-              height: 40,
-              child: Image.asset(AppImages.blocks),
+            Expanded(
+              flex: 1,
+              child: Text(
+                completed,
+                style: AppTextStyles.body11,
+              ),
             ),
-            SizedBox(
-              height: 20,
-            ),
-            Text("Gerenciamento de Estado", style: AppTextStyles.heading15),
-            SizedBox(
-              height: 20,
-            ),
-            Row(children: [
-              Expanded(
-                  flex: 1, child: Text("3/10", style: AppTextStyles.body11)),
-              Expanded(
+            Expanded(
                 flex: 4,
-                child: ProgressIndicatorWidget(value: 0.3),
-              )
-            ])
+                child: ProgressIndicatorWidget(
+                  value: percent,
+                ))
           ],
-        ));
+        )
+      ]),
+    );
   }
 }
