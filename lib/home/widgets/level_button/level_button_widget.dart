d617ddcfe6256801077122bcfeb888bc00be7cc9
Paulo Vieira <paullocostta86@gmail.com>
2021-04-21 21:13:25 -0300

    feat: alter level buttons

--- lib/home/widgets/level_button/level_button_widget.dart
+++ lib/home/widgets/level_button/level_button_widget.dart
@@ -4,10 +4,8 @@ import 'package:google_fonts/google_fonts.dart';
 
 class LevelButtonWidget extends StatelessWidget {
   final String label;
-  LevelButtonWidget({
-    Key? key,
-    required this.label,
-  })   : assert(["Fácil", "Médio", "Difícil", "Períto"].contains(label)),
+  LevelButtonWidget({Key? key, required this.label})
+      : assert(["Fácil", "Médio", "Difícil", "Perito"].contains(label)),
         super(key: key);
 
   final config = {
@@ -26,7 +24,7 @@ class LevelButtonWidget extends StatelessWidget {
       "borderColor": AppColors.levelButtonBorderDificil,
       "fontColor": AppColors.levelButtonTextDificil,
     },
-    "Períto": {
+    "Perito": {
       "color": AppColors.levelButtonPerito,
       "borderColor": AppColors.levelButtonBorderPerito,
       "fontColor": AppColors.levelButtonTextPerito,
@@ -40,17 +38,17 @@ class LevelButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-            color: color,
-            border: Border.fromBorderSide(BorderSide(color: borderColor)),
-            borderRadius: BorderRadius.circular(28)),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 26, vertical: 6),
-          child: Text(label,
-              style: GoogleFonts.notoSans(
-                color: fontColor,
-                fontSize: 13,
-              )),
-        ));
+      decoration: BoxDecoration(
+          color: color,
+          border: Border.fromBorderSide(BorderSide(
+            color: borderColor,
+          )),
+          borderRadius: BorderRadius.circular(28)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 26, vertical: 6),
+        child: Text(label,
+            style: GoogleFonts.notoSans(color: fontColor, fontSize: 13)),
+      ),
+    );
   }
 }
