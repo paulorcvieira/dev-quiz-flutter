b2e13eefa0fe9d52b7aeeb5706c5205adcc812f5
Paulo Vieira <paullocostta86@gmail.com>
2021-04-22 14:43:50 -0300

    refactor: get list questions from database

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,11 +1,17 @@
 import 'package:devquiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:devquiz/challenge/widgets/quiz/quiz_widget.dart';
+import 'package:devquiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
 import 'widgets/next_button/next_button_widget.dart';
 
 class ChallengePage extends StatefulWidget {
-  ChallengePage({Key? key}) : super(key: key);
+  final List<QuestionModel> questions;
+
+  ChallengePage({
+    Key? key,
+    required this.questions,
+  }) : super(key: key);
 
   @override
   _ChallengePageState createState() => _ChallengePageState();
@@ -34,7 +40,7 @@ class _ChallengePageState extends State<ChallengePage> {
         ),
       ),
       body: QuizWidget(
-        title: "O que o Flutter faz em sua totalidade?",
+        question: widget.questions[0],
       ),
       bottomNavigationBar: SafeArea(
         bottom: true,
