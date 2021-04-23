cbed91cf3d660531131b5f9dd165c6cf57e9d6f2
Paulo Vieira <paullocostta86@gmail.com>
2021-04-23 12:09:55 -0300

    refactor: confirm to go the result page

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,5 +1,6 @@
 import 'package:devquiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:devquiz/challenge/widgets/quiz/quiz_widget.dart';
+import 'package:devquiz/result/result_page.dart';
 import 'package:devquiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
@@ -97,7 +98,11 @@ class _ChallengePageState extends State<ChallengePage> {
                               child: NextButtonWidget.green(
                             label: "Confirmar",
                             onTap: () {
-                              Navigator.pop(context);
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => ResultPage(),
+                                  ));
                             },
                           )),
                       ],
